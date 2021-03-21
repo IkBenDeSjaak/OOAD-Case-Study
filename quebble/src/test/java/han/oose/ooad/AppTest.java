d1e8fba0fe0e5f2ce942e25a134924d8fe116ffc
Sjaak <sjaakrest@gmail.com>
2021-03-21 20:38:19 +0100

    add java project for quebble

---
+++ quebble/src/test/java/han/oose/ooad/AppTest.java
@@ -0,0 +1,20 @@
+package han.oose.ooad;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
